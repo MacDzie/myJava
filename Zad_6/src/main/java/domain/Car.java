9d94a2fe5890c627c5dd4804c891d65c12646e26
MacDzie <maciej.dzieciuch@wp.pl>
2019-05-31 22:57:52 +0200

    update

---
+++ Zad_6/src/main/java/domain/Car.java
@@ -0,0 +1,38 @@
+package domain;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String model;
+
+    @ManyToOne
+    private Person person;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+}
